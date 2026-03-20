7610c4c263aec53a80c4a2ab5cf7e8d7aaae4aa2
Pasha <Pasha@LAPTOP-5Q537UU6>
2026-03-20 15:25:37 +0300

    Добавлен ComboBox для выбора языка

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -20,5 +20,16 @@ namespace Lab_MVVM
         {
             InitializeComponent();
         }
+
+        private void LanguageComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (LanguageComboBox.SelectedItem is ComboBoxItem selectedItem)
+            {
+                string lang = selectedItem.Tag.ToString();
+                // Здесь в ветке loc-library мы пропишем переключение для CodinSeb
+                // CodinSeb.Localization.LocalizationProvider.Instance.CurrentLanguage = lang;
+            }
+        }
+
     }
 }
