fbdab77e295cb1b5f95e1480893e18711c661fe5
Pasha <Pasha@LAPTOP-5Q537UU6>
2026-03-20 18:21:15 +0300

    Использование словарей ресурсов XAML (ResourceDictionary)

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -26,9 +26,30 @@ namespace Lab_MVVM
             if (LanguageComboBox.SelectedItem is ComboBoxItem selectedItem)
             {
                 string lang = selectedItem.Tag.ToString();
-                // Здесь в ветке loc-library мы пропишем переключение для CodinSeb
-                // CodinSeb.Localization.LocalizationProvider.Instance.CurrentLanguage = lang;
+                ChangeLanguage(lang);
             }
+        }
+
+        private void ChangeLanguage(string lang)
+        {
+            // Находим старый словарь ресурсов (ru или en)
+            ResourceDictionary oldDict = Application.Current.Resources.MergedDictionaries
+                .FirstOrDefault(d => d.Source != null && d.Source.OriginalString.Contains("Dict."));
+
+            if (oldDict != null)
+            {
+                Application.Current.Resources.MergedDictionaries.Remove(oldDict);
+            }
+
+            // Загружаем новый словарь
+            ResourceDictionary newDict = new ResourceDictionary();
+            newDict.Source = new Uri($"Langs/Dict.{lang}.xaml", UriKind.Relative);
+
+            Application.Current.Resources.MergedDictionaries.Add(newDict);
+
+            // Пример перевода сообщения из кода (Задание п.2)
+            string welcomeMsg = Application.Current.TryFindResource("m_Welcome") as string;
+            // MessageBox.Show(welcomeMsg); 
         }
 
     }
